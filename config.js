7a568df5362feab44c08fedeb40673d7af88bf5c
edgarucho <edgarv0211@gmail.com>
2026-09-04 11:10:54 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// ============================================
+//  CONFIGURACION — ALUM-NA
+//  Cambia esta linea. Nada mas.
+// ============================================
+
+// La URL del web app de Apps Script. Termina en /exec
+const API_URL = 'https://script.google.com/macros/s/AKfycbydPDWks9H6U0DFomh-7XBJU7v4l-5912HDAjK94MRfbBhIfx7rhwxY0QM9wgB-GnQC/exec';
