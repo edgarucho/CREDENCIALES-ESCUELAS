5469d65eb95e5c7ef1f94105040aca72f682d6b5
edgarucho <edgarv0211@gmail.com>
2026-09-04 11:10:28 -0600

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// ============================================
-//  CONFIGURACION — ALUM-NA
-//  Cambia esta linea. Nada mas.
-// ============================================
-
-// La URL del web app de Apps Script. Termina en /exec
-const API_URL = 'https://script.google.com/macros/s/AKfycbydPDWks9H6U0DFomh-7XBJU7v4l-5912HDAjK94MRfbBhIfx7rhwxY0QM9wgB-GnQC/exec';
