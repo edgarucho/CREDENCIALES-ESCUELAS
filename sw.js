29066d9548d75838c44543dbe7e971616fa32af0
edgarucho <edgarv0211@gmail.com>
2026-09-23 10:19:15 -0600

    Update service worker version to alumna-v38

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Sube el numero de abajo cada vez que cambie index.html.
    ============================================================ */
 
-var VERSION = 'alumna-v37';
+var VERSION = 'alumna-v38';
 
 var BASICOS = [
   './',
