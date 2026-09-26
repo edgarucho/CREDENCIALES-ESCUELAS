389a6dedfd30ee1a29e3f1459745eace37ae3d86
edgarucho <edgarv0211@gmail.com>
2026-09-25 19:13:02 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Sube el numero de abajo cada vez que cambie index.html.
    ============================================================ */
 
-var VERSION = 'alumna-v50';
+var VERSION = 'alumna-v51';
 
 var BASICOS = [
   './',
